6105530ed2b4a46b8ba2f333789a7a51182b9a0c
rneumann <rneumann@br.ibm.com>
2025-12-09 18:20:20 -0300

    Add runs/ output

---
+++ sqd-lattice/runs/hafnium_2/dft_data/hubbard_params.py
@@ -0,0 +1,7 @@
+# Intra-orbital Hubbard U correction 
+U_hubbard = {'Hf5D': 2.6590} 
+
+# Inter-orbital Hubbard V correction
+V_hubbard = { ('Hf5D','O2P'): 0.5672}
+# Number of electrons per atom
+ne_ab = (12, 12)
---
+++ sqd-lattice/runs/zirconia_2/dft_data/hubbard_params.py
@@ -0,0 +1,7 @@
+# Intra-orbital Hubbard U correction 
+U_hubbard = {'Zr4D': 3.1334} 
+
+# Inter-orbital Hubbard V correction
+V_hubbard = { ('Zr4D','O2P'): 0.6392}
+# Number of electrons per atom
+ne_ab = (12, 12)
